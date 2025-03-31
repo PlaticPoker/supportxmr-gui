9dd4b44ac71b906f28ef4edcb690a6e82d652ce1
PlaticPoker <Gilbertoupt@gmail.com>
2025-03-31 22:59:43 +0100

    Update script_min.js

--- script_min.js
+++ script_min.js
@@ -1362,12 +1362,7 @@ var api = function(m, key, xid){
 				params = JSON.stringify(params);
 			}
 			
-			if(m === 'net'){
-				xhr.open('GET', 'https://supportxmr.com/api/'+url, true);
-			}else{
-				xhr.open('GET', $Q['api']+url, true);
-			}
-			//xhr.open(method, $Q['api']+url, true);
+xhr.open('GET', $Q['api'] + url, true);
 			xhr.setRequestHeader('Content-Type', 'application/json');
 			
 			if(method === 'POST'){
